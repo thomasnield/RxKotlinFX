76f6f771cb30324e8ceabe5e04c86a25e2b667ae
Thomas Nield <thomasnield@live.com>
2016-02-15 00:12:11 -0600

    more interop extensions

--- src/main/kotlin/rx/javafx/kt/RxFxExtension.kt
+++ src/main/kotlin/rx/javafx/kt/RxFxExtension.kt
@@ -1,8 +1,12 @@
 package rx.javafx.kt
 
 import javafx.beans.value.ObservableValue
+import javafx.event.Event
+import javafx.event.EventType
+import javafx.scene.Node
 import rx.Observable
 import rx.observables.JavaFxObservable
+import rx.schedulers.JavaFxScheduler
 import rx.subscribers.JavaFxSubscriber
 
 
@@ -10,3 +14,5 @@ fun <T> Observable<T>.toBinding() = JavaFxSubscriber.toBinding(this)
 fun <T> Observable<T>.toBinding(errorHandler: (Throwable) -> Unit) = JavaFxSubscriber.toBinding(this,errorHandler)
 fun <T> ObservableValue<T>.toObservable() = JavaFxObservable.fromObservableValue(this)
 fun <T> ObservableValue<T>.toObservableChanges() = JavaFxObservable.fromObservableValueChanges(this)
+fun <T> Observable<T>.toFxScheduler() = observeOn(JavaFxScheduler.getInstance())
+fun <T : Event> Node.toNodeEvents(eventType: EventType<T>) = JavaFxObservable.fromNodeEvents(this, eventType)

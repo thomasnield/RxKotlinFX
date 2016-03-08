54bbd3da1b7c9e6e45c2704c8daf3b21163dfd36
Thomas Nield <thomasnield@live.com>
2016-03-08 17:23:11 -0600

    Exploring TableView reactive extensions. RxJavaFX may need to work with ObservableCollections

--- src/main/kotlin/rx/javafx/kt/Nodes.kt
+++ src/main/kotlin/rx/javafx/kt/Nodes.kt
@@ -1,12 +1,17 @@
 package rx.javafx.kt
 
-import javafx.event.EventType
-import javafx.scene.Node
-import javafx.scene.input.MouseEvent
-import rx.observables.JavaFxObservable
+import javafx.scene.control.TableView
+import rx.Observable
 
-fun <N: Node> N.mouseEvents(mouseEventType: EventType<MouseEvent>) =
-        JavaFxObservable.fromNodeEvents(this,mouseEventType)
 
-fun <N: Node> N.mouseClicks() =
-        JavaFxObservable.fromNodeEvents(this,MouseEvent.MOUSE_CLICKED)
+val <T> TableView<T>.rowSelections: Observable<Int>
+    get() = itemSelections.map { selectionModel.selectedIndex }
+
+/*
+val <T> TableView<T>.columnSelections: Observable<Int>
+    get() = selectionModel.selectedcell
+*/
+
+val <T> TableView<T>.itemSelections: Observable<T>
+    get() = this.selectionModel.selectedItemProperty().toObservable()
+

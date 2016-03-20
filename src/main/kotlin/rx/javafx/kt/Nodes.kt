1329ae395421e5394d5aa9ae02b0031301afc93c
Thomas Nield <thomasnield@live.com>
2016-03-19 21:20:52 -0500

    Start implementing Nodes for TableView and ListView

--- src/main/kotlin/rx/javafx/kt/Nodes.kt
+++ src/main/kotlin/rx/javafx/kt/Nodes.kt
@@ -1,17 +1,23 @@
 package rx.javafx.kt
 
+import javafx.scene.control.ListView
 import javafx.scene.control.TableView
 import rx.Observable
 
 
-val <T> TableView<T>.rowSelections: Observable<Int>
+val <T> TableView<T>.rowIndexSelections: Observable<Int>
     get() = itemSelections.map { selectionModel.selectedIndex }
 
-/*
-val <T> TableView<T>.columnSelections: Observable<Int>
-    get() = selectionModel.selectedcell
-*/
+
+val <T> TableView<T>.columnIndexSelections: Observable<Int>
+    get() = selectionModel.selectedCells.toObservable().flatMap { Observable.from(it).map { it.column } }
+
 
 val <T> TableView<T>.itemSelections: Observable<T>
-    get() = this.selectionModel.selectedItemProperty().toObservable()
+    get() = selectionModel.selectedItemProperty().toObservable()
+
+
+val <T> ListView<T>.itemSelections: Observable<T>
+    get() = selectionModel.selectedItems.toObservable().flatMap { Observable.from(it) }
+
 

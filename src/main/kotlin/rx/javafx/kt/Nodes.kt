1c222b7e7828c3cbbc5df807626a24c520d42e72
Thomas Nield <thomasnield@live.com>
2016-03-24 11:24:13 -0500

    Add ComboBox#valueSelections

--- src/main/kotlin/rx/javafx/kt/Nodes.kt
+++ src/main/kotlin/rx/javafx/kt/Nodes.kt
@@ -1,31 +1,38 @@
 package rx.javafx.kt
 
+import javafx.scene.control.ComboBox
 import javafx.scene.control.ListView
 import javafx.scene.control.TableView
 import rx.Observable
 
 /**
- * Returns an Observable emitting integer values for selected row indexes.
+ * Returns an [Observable] emitting integer values for selected row indexes.
  */
 val <T> TableView<T>.rowIndexSelections: Observable<Int>
     get() = itemSelections.map { selectionModel.selectedIndex }
 
 /**
- * Returns an Observable emitting integer values for selected column indexes.
+ * Returns an [Observable] emitting integer values for selected column indexes.
  */
 val <T> TableView<T>.columnIndexSelections: Observable<Int>
     get() = selectionModel.selectedCells
             .additions().map { it.column }
 
 /**
- * Returns an Observable emitting selected items for the given TableView
+ * Returns an [Observable] emitting selected items for the given TableView
  */
 val <T> TableView<T>.itemSelections: Observable<T>
     get() = selectionModel.selectedItemProperty().toObservable()
 
 /**
- * Returns an Observable emitting single selected items for the given ListView
+ * Returns an [Observable] emitting single selected items for the given [ListView]
  */
 val <T> ListView<T>.itemSelections: Observable<T>
     get() = selectionModel.selectedItems.additions()
+
+/**
+ * Returns an [Observable] emitting single selected items for the given [ComboBox]
+ */
+val <T> ComboBox<T>.valueSelections: Observable<T>
+    get() = valueProperty().toObservable()
 

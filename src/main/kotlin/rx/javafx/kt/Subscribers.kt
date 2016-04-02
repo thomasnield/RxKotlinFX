62964453ef202f9c348abb4d007282599a38c414
Thomas Nield <thomasnield@live.com>
2016-04-01 22:23:52 -0500

    add JavaDocs

--- src/main/kotlin/rx/javafx/kt/Subscribers.kt
+++ src/main/kotlin/rx/javafx/kt/Subscribers.kt
@@ -3,4 +3,9 @@ package rx.javafx.kt
 import javafx.beans.property.Property
 import rx.Observable
 
+
+/**
+ * Binds the `Property` to an RxJava `Observable`,
+ * meaning it will be bounded to show the latest emissions of that `Observable`
+ */
 fun <T> Property<T>.bind(observable: Observable<T>) = bind(observable.toBinding())

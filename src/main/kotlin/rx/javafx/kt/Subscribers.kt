ba30143b914e6dd03a45d2b5ed62458bce41b533
Thomas Nield <thomasnield@live.com>
2016-04-03 12:49:34 -0500

    make Property#bind() return the Binding

--- src/main/kotlin/rx/javafx/kt/Subscribers.kt
+++ src/main/kotlin/rx/javafx/kt/Subscribers.kt
@@ -1,11 +1,18 @@
 package rx.javafx.kt
 
+import javafx.beans.binding.Binding
 import javafx.beans.property.Property
 import rx.Observable
 
 
 /**
  * Binds the `Property` to an RxJava `Observable`,
- * meaning it will be bounded to show the latest emissions of that `Observable`
+ * meaning it will be bounded to show the latest emissions of that `Observable`.
+ * The `Binding` is also returned so caller can be dispose it later if needed
+ * @return `Binding`
  */
-fun <T> Property<T>.bind(observable: Observable<T>) = bind(observable.toBinding())
+fun <T> Property<T>.bind(observable: Observable<T>): Binding<T> {
+    val binding = observable.toBinding()
+    bind(binding)
+    return binding
+}

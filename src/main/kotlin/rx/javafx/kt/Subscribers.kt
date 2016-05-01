96a54a55b8035d73aa5069c982c0e42a84ec7af2
Thomas Nield <thomasnield@live.com>
2016-05-01 13:16:02 -0500

    Implement extensions to CompositeBinding

--- src/main/kotlin/rx/javafx/kt/Subscribers.kt
+++ src/main/kotlin/rx/javafx/kt/Subscribers.kt
@@ -3,6 +3,7 @@ package rx.javafx.kt
 import javafx.beans.binding.Binding
 import javafx.beans.property.Property
 import rx.Observable
+import rx.subscriptions.CompositeBinding
 
 
 /**
@@ -16,3 +17,20 @@ fun <T> Property<T>.bind(observable: Observable<T>): Binding<T> {
     bind(binding)
     return binding
 }
+
+/**
+ * Add this `Binding` to the provided `CompositeBinding`, and returns itself
+ * @return `Binding`
+ */
+fun <T> Binding<T>.addto(compositeBinding: CompositeBinding): Binding<T> {
+    compositeBinding.add(this)
+    return this
+}
+
+operator fun <T> CompositeBinding.plusAssign(binding: Binding<T>) = add(binding)
+
+operator fun CompositeBinding.plusAssign(compositeBinding: CompositeBinding) = add(compositeBinding)
+
+operator fun <T> CompositeBinding.minusAssign(binding: Binding<T>) = remove(binding)
+
+operator fun CompositeBinding.minusAssign(compositeBinding: CompositeBinding) = remove(compositeBinding)

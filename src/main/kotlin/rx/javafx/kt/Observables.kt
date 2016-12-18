3139e6957c686699328408911649b6391b5d6f4b
Thomas Nield <thomasnield@live.com>
2016-12-18 12:39:39 -0600

    implement non-null factories for ObservableValue

--- src/main/kotlin/rx/javafx/kt/Observables.kt
+++ src/main/kotlin/rx/javafx/kt/Observables.kt
@@ -47,12 +47,29 @@ fun <T> Observable<T>.toLazyBinding(errorHandler: (Throwable) -> Unit) = JavaFxS
  * @return an Observable emitting values as the wrapped ObservableValue changes
  */
 fun <T> ObservableValue<T>.toObservable() = JavaFxObservable.valuesOf(this)
+
+
+/**
+ * Create an rx Observable from a javafx ObservableValue
+ * @param <T>          the type of the observed value
+ * @return an Observable emitting non-null values as the wrapped ObservableValue changes
+ */
+fun <T> ObservableValue<T>.toObservableNonNull() = JavaFxObservable.nonNullValuesOf(this)
+
 /**
  * Create an rx Observable from a javafx ObservableValue, and emits changes with old and new value pairs
  * @param <T>          the type of the observed value
  * @return an Observable emitting values as the wrapped ObservableValue changes
  */
 fun <T> ObservableValue<T>.toObservableChanges() = JavaFxObservable.changesOf(this)
+
+/**
+ * Create an rx Observable from a javafx ObservableValue, and emits changes with old and new non-null value pairs
+ * @param <T>          the type of the observed value
+ * @return an Observable emitting non-null values as the wrapped ObservableValue changes
+ */
+fun <T> ObservableValue<T>.toObservableChangesNonNull() = JavaFxObservable.nonNullChangesOf(this)
+
 
 /**
  * Creates an observable corresponding to javafx ContextMenu action events.

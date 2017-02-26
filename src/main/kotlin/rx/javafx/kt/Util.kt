c302c29e7852a907ab2f8b668ecea9bd0fb373da
Thomas Nield <thomasnield@live.com>
2017-02-25 20:22:20 -0600

    drop files

--- src/main/kotlin/rx/javafx/kt/Util.kt
+++
@@ -1,8 +0,0 @@
-package rx.javafx.kt
-
-import io.reactivex.Observable
-import java.util.Optional
-
-fun <T> Optional<T>.toObservable() = Observable.just(this)
-        .filter { it.isPresent }
-        .map { it.get() }
